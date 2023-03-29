2b6d44fc0f8e37b2bce83df7fdd65fc570211a81
DariaGood <102043020+DariaGood@users.noreply.github.com>
2023-03-29 19:07:58 +0300

    ready2

--- src/js/fetchimages.js
+++ src/js/fetchimages.js
@@ -4,11 +4,11 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '32810217-d4150f11c342a4e2afb80e8cd';
 //id 32810217
 export async function fetchImages(value, page = 1, perPage = 40) {
-  let response;
   try {
-    return response = await axios.get(
+     const response = await axios.get(
       `${BASE_URL}?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
-    );
+      )
+      return response;
  
   } catch (error) {
     return Promise.reject(`${error}`);
